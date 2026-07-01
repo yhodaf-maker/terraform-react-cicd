8bcbb9bbf384503c98a5eba7c8be1d8db59d0ea0
Yehuda feder <yhodafeder@gmail.com>
2026-07-01 11:02:50 +0300

    Format Terraform files

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "website" {
-  bucket = var.bucket_name
+  bucket        = var.bucket_name
   force_destroy = true
 
   tags = {
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,2 +1,2 @@
-aws_region = "eu-west-1"
+aws_region  = "eu-west-1"
 bucket_name = "yhodaf-cloudacademy-react-2026"
